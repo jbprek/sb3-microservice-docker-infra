f4cf255b3f38970c4d16d94dcd7ff1e529e7eaaa
jbprek <jbprek@gmail.com>
2024-10-12 08:21:56 +0100

    Adding config-service

--- eureka-service/src/main/java/com/foo/eureka/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/foo/eureka/EurekaServiceApplication.java
@@ -1,4 +1,4 @@
-package com.foo.emp.springcloudeurekaserver;
+package com.foo.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
